e35b62e30cc1b808203539a6acc7dad6a6019230
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-28 13:02:32 +0100

    LiveCoding

---
+++ LiveCoding/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LiveCoding;
+using LiveCoding.BonusGeometria;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Inserisci L'altezza dell Rettangolo");
+int altezzaRettangolo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inserisci La Base del Rettangolo");
+int baseRettangolo = Convert.ToInt32(Console.ReadLine());
+
+Rettangolo InputUtente = new Rettangolo(altezzaRettangolo, baseRettangolo);
+
+InputUtente.StampaTuttiIDati();
+
---
+++ LiveCoding/Rettangolo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCoding
+{
+    namespace BonusGeometria
+    {
+        public class Rettangolo
+        {
+            public int baseRettangolo;
+            public int altezzaRettangolo;
+
+            // Posso definire un altro COSTRUTTORE customizzato usando ancora l'OVERLOAD
+            public Rettangolo(int altezzaRettangolo, int baseRettangolo)
+            {
+                this.baseRettangolo = baseRettangolo;
+                this.altezzaRettangolo = altezzaRettangolo;
+            }
+
+            public int calcolaArea()
+            {
+                int area = baseRettangolo * altezzaRettangolo;
+                return area;
+            }
+            public int calcolaPerimetro()
+            {
+                int perimetro = (baseRettangolo + altezzaRettangolo) * 2;
+                return perimetro;
+            }
+
+            public void StampaTuttiIDati()
+            {
+                int area = baseRettangolo * altezzaRettangolo;
+                int perimetro = (baseRettangolo + altezzaRettangolo) * 2;
+                Console.WriteLine($"Base: {baseRettangolo}");
+                Console.WriteLine($"Altezza: {altezzaRettangolo}");
+                Console.WriteLine($"Perimetro: {calcolaPerimetro()}");
+                Console.WriteLine($"Area: {calcolaArea()}");
+            }
+
+
+
+
+
+
+
+            public void StampaDisegno()
+            {
+                Console.Write("\"");
+                for (int i = 0; i < baseRettangolo - 2; i++)
+                {
+                    Console.Write("\"");
+                }
+                Console.WriteLine("\"");
+
+                for (int i = 0; i < altezzaRettangolo; i++)
+                {
+                    Console.Write("|");
+                    for (int b = 0; b < baseRettangolo - 2; b++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("|");
+                }
+                Console.Write("\"");
+                for (int i = 0; i < baseRettangolo - 2; i++)
+                {
+                    Console.Write("\"");
+                }
+                Console.WriteLine("\"");
+            }
+
+        }
+    }
+
+}
